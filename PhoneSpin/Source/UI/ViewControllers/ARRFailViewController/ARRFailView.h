bff8f1da3549b113dc7595880d35d887681dea8e
RunesReader <1000000000g@gmail.com>
2015-12-03 21:06:26 +0200

    Adds ARRFailView.

---
+++ PhoneSpin/Source/UI/ViewControllers/ARRFailViewController/ARRFailView.h
@@ -0,0 +1,13 @@
+//
+//  ARRFailView.h
+//  PhoneSpin
+//
+//  Created by Igor Arsenkin on 03.12.15.
+//  Copyright © 2015 Igor Arsenkin. All rights reserved.
+//
+
+#import "ARRView.h"
+
+@interface ARRFailView : ARRView
+
+@end
---
+++ PhoneSpin/Source/UI/ViewControllers/ARRFailViewController/ARRFailView.m
@@ -0,0 +1,13 @@
+//
+//  ARRFailView.m
+//  PhoneSpin
+//
+//  Created by Igor Arsenkin on 03.12.15.
+//  Copyright © 2015 Igor Arsenkin. All rights reserved.
+//
+
+#import "ARRFailView.h"
+
+@implementation ARRFailView
+
+@end
